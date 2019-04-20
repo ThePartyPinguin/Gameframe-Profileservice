94a3a21d74d3b50b09e87806bc737b172f1ea19b
Joris Eijmberts <j.eijmberts@student.fontys.nl>
2019-04-21 00:23:25 +0200

    Updated pom, added config basic config, updated main class

--- src/main/java/profileservice/GameFrameProfileService.java
+++ src/main/java/profileservice/GameFrameProfileService.java
@@ -1,4 +1,24 @@
 package profileservice;
 
-public class GameFrameProfileService {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class GameFrameProfileService extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GameFrameProfileService.class);
+    }
+
+
+    public static void main(String[] args){
+        SpringApplication.run(GameFrameProfileService.class, args);
+    }
 }
